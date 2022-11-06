cd8465f95cfcc9c7476717dbf7375521be1c2ccc
slavaIv <slavchiky@gmail.com>
2022-11-06 20:30:47 +0100

    checkout box was added(shipping address, billing address, customer info, credit card info, total)

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+    totalPrice: number = 0;
+    totalQuantity: number = 0;
+
+    checkoutFormGroup: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.checkoutFormGroup = this.formBuilder.group({
+            customer: this.formBuilder.group({
+                firstName: [''],
+                lastName: [''],
+                email: ['']
+            }),
+            shippingAddress: this.formBuilder.group({
+                country: [''],
+                street: [''],
+                city: [''],
+                state: [''],
+                zipCode: ['']
+            }),
+            billingAddress: this.formBuilder.group({
+                country: [''],
+                street: [''],
+                city: [''],
+                state: [''],
+                zipCode: ['']
+            }),
+            creditCard: this.formBuilder.group({
+                cardType: [''],
+                cardName: [''],
+                cardNumber: [''],
+                securityCode: [''],
+                expMonth: [''],
+                expYear: ['']
+            })
+       });
+    }
+
+    onSubmit() {
+        console.log("Handle submit button");
+        console.log(this.checkoutFormGroup.get('customer').value);
+    }
+
+    copyShippingAddressToBillingAddress(event) {
+        if(event.target.checked) {
+            this.checkoutFormGroup.controls['billingAddress'].setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+        }
+        else {
+            this.checkoutFormGroup.controls['billingAddress'].reset();
+        }
+    }
+
+}
